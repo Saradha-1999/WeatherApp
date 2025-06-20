ac93d7074ca800650dc60ef6c7fbbee5f52ba2c8
Saradha <fantastic4.4c@gmail.com>
2025-06-20 14:04:19 +0530

    Update for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
+
+import React from 'react'
+import Weather from './components/Weather'
+
 const App = () => {
   return (
-    <div style={{ fontSize: '2rem', padding: '2rem' }}>
-      App is working!
+    <div className='app'>
+      <Weather/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
-
-
-
-// import React from 'react'
-// import Weather from './components/Weather'
-
-// const App = () => {
-//   return (
-//     <div className='app'>
-//       <Weather/>
-//     </div>
-//   )
-// }
-
-// export default App
+export default App
