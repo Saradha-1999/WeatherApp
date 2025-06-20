7bc6efac2ba3a62f13334ee8f276c7982761660e
Saradha <fantastic4.4c@gmail.com>
2025-06-20 13:46:03 +0530

    Test without weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import Weather from './components/Weather'
-
 const App = () => {
   return (
-    <div className='app'>
-      <Weather/>
+    <div style={{ fontSize: '2rem', padding: '2rem' }}>
+      App is working!
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+
+
+// import React from 'react'
+// import Weather from './components/Weather'
+
+// const App = () => {
+//   return (
+//     <div className='app'>
+//       <Weather/>
+//     </div>
+//   )
+// }
+
+// export default App
