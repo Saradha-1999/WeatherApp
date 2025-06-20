a7d9d6eb0f87b8f03bc792e1ab4e17b18564aad6
Saradha <fantastic4.4c@gmail.com>
2025-06-20 12:46:45 +0530

    Try bas '/'

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // ✅ This is the fix for Netlify
+  base: '/', // ✅ This is the fix for Netlify
   plugins: [react()],
 });
